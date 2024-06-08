04199af3f10cb001594cbb878c276d7298ee4b66
Cesar Santos <cesarsantostorres@gmail.com>
2024-06-08 13:45:37 -0500

    Temporarily disable the use of Spring Data JPA

--- src/main/java/pe/edu/cibertec/app_services_demo/repository/VehicleRepository.java
+++ src/main/java/pe/edu/cibertec/app_services_demo/repository/VehicleRepository.java
@@ -3,5 +3,5 @@ package pe.edu.cibertec.app_services_demo.repository;
 import org.springframework.data.repository.CrudRepository;
 import pe.edu.cibertec.app_services_demo.entity.Vehicle;
 
-public interface VehicleRepository extends CrudRepository<Vehicle, Long> {
+public interface VehicleRepository /*extends CrudRepository<Vehicle, Long>*/ {
 }
